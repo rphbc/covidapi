90f7f882b0d354c34ea1abb09dd541a8c10e026d
Raphael Costa <raphaelbc@gmail.com>
2020-03-22 19:48:31 -0300

    fix statics

--- covidapi/settings.py
+++ covidapi/settings.py
@@ -129,9 +129,9 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-STATIC_URL = '/staticfiles/'
+STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # LOGGING = {
