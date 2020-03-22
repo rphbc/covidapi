bb743ff66548836624b8dad70c93f76a0408c133
Raphael Costa <raphaelbc@gmail.com>
2020-03-22 18:33:08 -0300

    fix

--- covidapi/settings.py
+++ covidapi/settings.py
@@ -125,11 +125,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
-
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static')
 ]
+
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # LOGGING = {
 #     'version': 1,
